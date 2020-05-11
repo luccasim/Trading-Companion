362c9926c778070b0fbc31dabb603eaf87a670e9
owee <casimir.luc@gmail.com>
2020-05-11 17:04:08 +0400

    Initial Commit

---
+++ Trading CompanionTests/Trading_CompanionTests.swift
@@ -0,0 +1,34 @@
+//
+//  Trading_CompanionTests.swift
+//  Trading CompanionTests
+//
+//  Created by owee on 11/05/2020.
+//  Copyright © 2020 devios. All rights reserved.
+//
+
+import XCTest
+@testable import Trading_Companion
+
+class Trading_CompanionTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
