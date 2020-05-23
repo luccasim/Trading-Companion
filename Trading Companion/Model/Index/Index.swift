1d4aabf529f491d270c73e7d9e722572058b21a1
owee <casimir.luc@gmail.com>
2020-05-24 00:10:05 +0400

    add Index Entity

---
+++ Trading Companion/Model/Index/Index.swift
@@ -0,0 +1,14 @@
+//
+//  index.swift
+//  Trading Companion
+//
+//  Created by owee on 24/05/2020.
+//  Copyright © 2020 devios. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Index : NSManagedObject {
+    
+}
