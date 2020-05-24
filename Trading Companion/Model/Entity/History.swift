d98c8ba46a388ec1db1c5df8d2bd53dfa2e9ed24
owee <casimir.luc@gmail.com>
2020-05-24 18:33:57 +0400

    refacto History parsing

--- Trading Companion/Model/Entity/History.swift
+++ Trading Companion/Model/Entity/History.swift
@@ -12,3 +12,62 @@ import CoreData
 class History : NSManagedObject {
     
 }
+
+extension History {
+    
+    enum Errors : Error {
+        case jsonSerialization
+        case historyToDictonary
+    }
+    
+    enum Keys : String {
+        case meta       = "Meta Data"
+        case daily      = "Time Series (Daily)"
+        
+        case open       = "1. open"
+        case high       = "2. high"
+        case low        = "3. low"
+        case close      = "4. close"
+        case volume     = "5. volume"
+    }
+    
+    static func with(AlphavantageData data:Data) throws -> [History] {
+        
+        let json = try JSONSerialization.jsonObject(with: data, options: [])
+        
+        guard let dict = json as? [String:Any] else {
+            throw Errors.jsonSerialization
+        }
+        
+        guard let daily = dict[Keys.daily.rawValue] as? [String:Any] else {
+            throw Errors.historyToDictonary
+        }
+        
+        var histories : [History] = []
+        
+        daily.forEach({ dict in
+            
+            let key = dict.key
+            
+            if let value = dict.value as? [String:Any] {
+                
+                let history = History(context: AppDelegate.viewContext)
+                
+                let formater = DateFormatter()
+                formater.dateFormat = "yyyy-MM-dd"
+                
+                history.date = formater.date(from: key)
+                history.open = Double(value[Keys.open.rawValue] as? String ?? "0") ?? 0
+                history.high = Double(value[Keys.high.rawValue] as? String ?? "0") ?? 0
+                history.close = Double(value[Keys.close.rawValue] as? String ?? "0") ?? 0
+                history.low = Double(value[Keys.low.rawValue] as? String ?? "0") ?? 0
+                history.volume = Double(value[Keys.volume.rawValue] as? String ?? "0") ?? 0
+                
+                histories.append(history)
+            }
+        })
+            
+        return histories
+    }
+    
+}
---
+++ Trading CompanionTests/Model/HistoryTests.swift
@@ -0,0 +1,34 @@
+//
+//  HistoryTests.swift
+//  Trading CompanionTests
+//
+//  Created by owee on 24/05/2020.
+//  Copyright © 2020 devios. All rights reserved.
+//
+
+import XCTest
+@testable import Trading_Companion
+
+class HistoryTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    private func get(FileName:String,FileExtension:String) -> URL! {
+        return Bundle(for: type(of: self)).url(forResource: FileName, withExtension: FileExtension)
+    }
+    
+    func testInitFormAlphavantage() throws {
+        
+        let data        = try Data(contentsOf: self.get(FileName: "IBM_history", FileExtension: "json"))
+        let histories   = try History.with(AlphavantageData: data)
+        
+        print(histories)
+    }
+
+}
